de70661adbaab55701b9a354cb75b230aa2ed00a
大脇雄斗 <>
2019-03-28 21:43:14 +0900

    #9 テーブルへの追加処理
    テーブル追加の処理記載しましたがブラウザはエラー

--- webroot/finish.php
+++ webroot/finish.php
@@ -43,6 +43,32 @@ function __finish($db, &$response) {
 	foreach ($response['cart_items']as $item) {
 		item_update_saled($db, $item['item_id'], $item['amount']);
 	}
+	$sql=<<<EOM
+    INSERT INTO orders
+    (user_id, purchase_date)
+    VALUES (:user_id, NOW())
+    EOM;
+	$params = array(
+	    ':user_id' =>  $_SESSION['user']['id']
+	);
+	db_update($db, $sql, $params = array());
+
+	$order_id = $db->lastInsertId();
+	foreach($response['cart_items'] as $items){
+	    $sql=<<<EOM
+        INSERT INTO order_details
+        (order_id, item_id, purchase_price, amount)
+        VALUES (:order_id, :item_id, :purchase_price, :amount)
+        EOM;
+	    $params = array(
+	        ':order_id' =>  $order_id,
+	        ':items_id' =>  $items['item_id'],
+	        ':purchase_price' =>  $items['price'],
+	        ':amount' => $items['amount']
+	    );
+	    db_update($db, $sql, $params = array());
+	}
+
 	cart_clear($db, $_SESSION['user']['id']);
 
 	$response['result_msg'] = 'ご購入、ありがとうございました。';
